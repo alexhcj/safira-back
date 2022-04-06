d6e5aeef28ea363b93f1beefdad8501b2b387d6e
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-07 00:15:27 +0300

    connect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_SERVER}?retryWrites=true&w=majority`,
+    ),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
