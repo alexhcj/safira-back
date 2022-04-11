1d6e891ecdb1c5127ccf49312f04bc5e5ff92f15
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-11 16:24:44 +0300

    add prefix & log server port

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT || 3000);
+
+  app.setGlobalPrefix('api/v1');
+
+  await app.listen(process.env.PORT || 3000, () =>
+    console.log(`Server port: ${process.env.PORT}`),
+  );
 }
 bootstrap();
