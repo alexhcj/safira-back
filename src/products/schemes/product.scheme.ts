87ecb5ef258773b033eda56212fb963e89a0aa5e
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-11 16:26:00 +0300

    add products scheme

---
+++ src/products/schemes/product.scheme.ts
@@ -0,0 +1,55 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+@Schema({ collection: 'products' })
+export class Product {
+  @Prop({ required: true })
+  readonly name: string;
+
+  @Prop({ required: true })
+  readonly description: string;
+
+  @Prop({ required: true })
+  readonly price: number;
+
+  @Prop({ default: '' })
+  readonly img: string;
+
+  @Prop({ default: 0 })
+  readonly quantity: number;
+
+  @Prop()
+  readonly category: string;
+
+  @Prop({ default: 0 })
+  readonly popularity: string;
+
+  @Prop({ default: 0 })
+  readonly views: number;
+
+  @Prop({ default: 0 })
+  readonly rating: number;
+
+  @Prop()
+  readonly company: string;
+
+  @Prop()
+  readonly importCountry: string;
+
+  @Prop()
+  readonly shelfLife: Date;
+
+  @Prop()
+  readonly tags: string[];
+
+  @Prop()
+  readonly productTags: string[];
+
+  // TODO: change to objectId
+  @Prop()
+  readonly reviews: string;
+}
+
+export const ProductScheme = SchemaFactory.createForClass(Product);
