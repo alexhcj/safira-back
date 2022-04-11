abe8a45af69add18aebd5fb593075a9edddc1ddd
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-11 16:25:01 +0300

    add products dto

---
+++ src/products/dto/product.dto.ts
@@ -0,0 +1,59 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly price: number;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly img: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly quantity: number;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly category: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly popularity: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly views: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly rating: number;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly company: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly importCountry: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly shelfLife: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly tags: string[];
+
+  @IsString()
+  @IsNotEmpty()
+  readonly productTags: string[];
+}
