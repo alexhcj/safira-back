511d80af6ded69a44a508d77d3fd0a2d1676b908
alex_hcj <alexeykorolyov19@gmail.com>
2023-10-25 13:40:42 +0300

    update: post user profile populate

--- src/posts/schemes/post.scheme.ts
+++ src/posts/schemes/post.scheme.ts
@@ -2,14 +2,14 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, SchemaTypes } from 'mongoose';
 import { PostCategoryEnum } from '../enums/post-category.enum';
 import { Comment } from '../../comments/schemes/comment.scheme';
-import { User } from '../../users/schemes/user.scheme';
+import { Profile } from '../../profiles/schemes/profile.scheme';
 
 export type PostDocument = Post & Document;
 
 @Schema({ collection: 'posts', timestamps: true })
 export class Post {
-  @Prop({ required: true, type: SchemaTypes.ObjectId, ref: User.name })
-  readonly userId: User;
+  @Prop({ required: true, type: SchemaTypes.ObjectId, ref: Profile.name })
+  readonly user: Profile;
 
   @Prop({ required: true })
   readonly title: string;
